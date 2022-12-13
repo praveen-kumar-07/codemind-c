8ae88d9b47eab8276aba16e1c8244916922af064
praveen-kumar-07 <119918367+praveen-kumar-07@users.noreply.github.com>
2022-12-13 10:28:26 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a+b>10)     {         printf("%d",21-(a+b));     }     else     printf("-1"); }

--- Blackjack.c
+++ Blackjack.c
@@ -3,12 +3,10 @@ int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    if((a+b)>10)
+    if(a+b>10)
     {
         printf("%d",21-(a+b));
     }
     else
-    {
-        printf("-1");
-    }
+    printf("-1");
 }
