5e8467ce2e6de75d6d51ac983d8b14651782779d
praveen-kumar-07 <119918367+praveen-kumar-07@users.noreply.github.com>
2022-12-14 10:35:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,r,s=0,i=0,k=0,p,n;     scanf("%d",&a);     n=a;   //  printf("%d",a);      while(a!=0)     {         r=a%10;         s=s*10+r;         a=a/10;     }     //printf("%d",s);
        while(s!=0)
        {
            p=s%10;
            i++;
            k=pow((p),i)+k;
            s=s/10;
            
          //  printf("%d",s);
        }
        //printf("%d",k);
        if(k==n)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

---
+++ Disarium_number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,r,s=0,i=0,k=0,p,n;
+    scanf("%d",&a);
+    n=a;
+  //  printf("%d",a);
+     while(a!=0)
+    {
+        r=a%10;
+        s=s*10+r;
+        a=a/10;
+    }
+    //printf("%d",s);
+    
+    while(s!=0)
+    {
+        p=s%10;
+        i++;
+        k=pow((p),i)+k;
+        s=s/10;
+        
+      //  printf("%d",s);
+    }
+    //printf("%d",k);
+    if(k==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
