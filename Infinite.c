011697764ca695752bdff7dc96c40e2d9108b769
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2022-12-25 13:16:19 +0530

    #include<stdio.h> int main() {     int a,i;     while(a!=0)     {         scanf("%d",&a);         if(a!=-1)         {             printf("%d ",a*a);         }         else         break;
        }
    }

---
+++ Infinite.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    while(a!=0)
+    {
+        scanf("%d",&a);
+        if(a!=-1)
+        {
+            printf("%d
+",a*a);
+        }
+        else
+        break;
+        
+    }
+}
