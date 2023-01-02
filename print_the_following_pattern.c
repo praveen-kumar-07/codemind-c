1817e91985aca195703d737ff1cb1713c4347837
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-01-02 19:23:44 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=1;j<=i;j++)         {         printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,11 @@ int main()
 {
     int a,i,j;
     scanf("%d",&a);
-    for(i=a;i>0;i--)
+    for(i=1;i<=a;i++)
     {
-        for(j=a;j>0;j--)
+        for(j=1;j<=i;j++)
         {
-        printf("%d ",j);
+        printf("%d",j);
         }
         printf("
 ");
